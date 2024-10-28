485401f5a8c04146d473972be22b05961f7b8c14
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-28 21:33:52 +0530

    added background img

--- krushnaproject/src/pages/home.jsx
+++ krushnaproject/src/pages/home.jsx
@@ -1,7 +1,7 @@
 // src/pages/Home.jsx
 
-import backgroundImage from './../assets/financ_backimg.jpeg'; // Adjust the path as necessary
-import './../style/index.css'
+import backgroundImage from '../assets/financ_backimg.jpeg'; // Adjust the path as necessary
+
 function Home() {
   return (
     <div
@@ -12,9 +12,11 @@ function Home() {
       }}
     >
       <div className="absolute inset-0 bg-black opacity-50" />
-      <div className="relative z-10 text-white text-center p-5">
-        <h1 className="text-5xl  font-bold">Welcome to FinancePro</h1>
-        <p className="mt-4 text-lg">
+      <div className="relative z-10 text-center p-5">
+        <h1 className="text-5xl font-bold bg-gradient-to-r from-red-400 to-blue-600 text-transparent bg-clip-text animate-gradient">
+          Welcome to FinancePro
+        </h1>
+        <p className="mt-4 text-lg text-white">
           Manage your finances with ease and confidence.
         </p>
         <button className="mt-6 px-4 py-2 bg-blue-600 hover:bg-blue-500 rounded">
